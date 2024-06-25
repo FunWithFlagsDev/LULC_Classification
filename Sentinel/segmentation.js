c66e6f6c5e43c891a0a461b67ec9c8594642e815
FunWithFlagsDev <134786514+FunWithFlagsDev@users.noreply.github.com>
2024-06-25 14:33:33 -0400

    Create segmentation.js

---
+++ Sentinel/segmentation.js
@@ -0,0 +1,19 @@
+var imageModule = require('users/Jhonnattan_Oliveira/Mestrado:modulos_Sentinel2/2 - analise_pca');
+
+var image = imageModule.mosaic2;
+
+// Define the parameters for SNIC
+var segmentation = ee.Algorithms.Image.Segmentation.SNIC({
+  image: image,
+  compactness: 0,
+  connectivity: 8,
+  neighborhoodSize: 48,
+  size: 2,
+});
+
+// Visualizing the Generated Clusters
+Map.addLayer(segmentation.randomVisualizer(), {}, 'clusters', 0);
+print('5 - Final Image 3', segmentation);
+
+// Export the processed final image as an object
+exports.mosaic3 = segmentation;
